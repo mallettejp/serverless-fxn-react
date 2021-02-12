62878c7600307513a48b5fa789e0fb6af281ea6b
James Mallette <jamespmallette@gmail.com>
2021-02-12 13:17:45 -0500

    initial commit

---
+++ src/Product.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useParams } from 'react-router-dom';
+
+const Product = () => {
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState(null);
+  const { productID } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`/api/products?id=${productID}`);
+        setProduct(data);
+      } catch (error) {}
+      setLoading(false);
+    };
+    fetchData();
+  }, [productID]);
+
+  if (loading) {
+    return (
+      <section className="section section-center">
+        <h2>loading</h2>
+      </section>
+    );
+  }
+  const { fields } = product;
+  const { name, description, price, image } = fields;
+  console.log(name);
+
+  return (
+    <section className="section section-center">
+      <Link to="/" className="link">
+        back home
+      </Link>
+      <div>
+        <div className="title">
+          <h2>{name}</h2>
+          <div className="title-underline"></div>
+        </div>
+        <article className="single-product">
+          <img src={image[0].url} alt={name} className="single-product-img" />
+          <div>
+            <h5>{name}</h5>
+            <h5 className="price">${price}</h5>
+            <p>{description}</p>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
